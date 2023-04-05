d6fc7e858641b8507a5638149168a94513db5cbd
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-04-05 16:57:57 -0300

    feat: criando a estrutura inicial do cabeçalho

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -2,5 +2,30 @@
 </script>
 
 <template>
-    <h1>Header</h1>
+    <header class="bg-black flex items-center justify-around">
+        <div class="">
+            <img class="max-w-250px" src="/base-logotipo-solarbens.png" alt="">
+        </div>
+        <nav class="">
+            <ul class="flex list-container">
+                <a href="" class="">
+                    <li class="navigation">Página inicial</li>
+                </a>
+                <a href="" class="">
+                    <li class="navigation">Quem somos</li>
+                </a>
+                <a href="" class="">
+                    <li class="navigation">Projetos</li>
+                </a>
+                <a href="" class="">
+                    <li class="navigation">Seja um parceiro</li>
+                </a>
+            </ul>
+        </nav>
+        <div class="">
+            <a href="" class="">
+                <button class="bg-solarYellow1 w-40 h-8 rounded-full font-semibold hover:bg-yellow-100 duration-300">Simule grátis</button>
+            </a>
+        </div>
+    </header>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     "./src/**/*.{vue, js, ts, jsx, tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        "80px": "5rem"
+      },
+      maxWidth: {
+        "250px": "15.625rem"
+      },
+      colors: {
+        solarYellow1: "#FFCC39",
+        solarBgHover: "#F7A811"
+      }
+    },
   },
   plugins: [],
 }
